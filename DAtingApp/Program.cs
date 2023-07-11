bc69e3bf2e44acad483e8a97173b8cb576b79b4c
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-07-11 11:06:43 +0300

    before-deployment

--- DAtingApp/Program.cs
+++ DAtingApp/Program.cs
@@ -31,31 +31,31 @@ namespace DAtingApp
 			
 			builder.Services.AddIdentityService(builder.Configuration);
 
-			//var connString = "";
-			//if (builder.Environment.IsDevelopment())
-			//	connString = builder.Configuration.GetConnectionString("co1");
-			//else
-			//{
-			//	// Use connection string provided at runtime by FlyIO.
-			//	var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+			var connString = "";
+			if (builder.Environment.IsDevelopment())
+				connString = builder.Configuration.GetConnectionString("co1");
+			else
+			{
+				// Use connection string provided at runtime by FlyIO.
+				var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-			//	// Parse connection URL to connection string for Npgsql
-			//	connUrl = connUrl.Replace("postgres://", string.Empty);
-			//	var pgUserPass = connUrl.Split("@")[0];
-			//	var pgHostPortDb = connUrl.Split("@")[1];
-			//	var pgHostPort = pgHostPortDb.Split("/")[0];
-			//	var pgDb = pgHostPortDb.Split("/")[1];
-			//	var pgUser = pgUserPass.Split(":")[0];
-			//	var pgPass = pgUserPass.Split(":")[1];
-			//	var pgHost = pgHostPort.Split(":")[0];
-			//	var pgPort = pgHostPort.Split(":")[1];
+				// Parse connection URL to connection string for Npgsql
+				connUrl = connUrl.Replace("postgres://", string.Empty);
+				var pgUserPass = connUrl.Split("@")[0];
+				var pgHostPortDb = connUrl.Split("@")[1];
+				var pgHostPort = pgHostPortDb.Split("/")[0];
+				var pgDb = pgHostPortDb.Split("/")[1];
+				var pgUser = pgUserPass.Split(":")[0];
+				var pgPass = pgUserPass.Split(":")[1];
+				var pgHost = pgHostPort.Split(":")[0];
+				var pgPort = pgHostPort.Split(":")[1];
 
-			//	connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-			//}
-			//builder.Services.AddDbContext<DataContext>(opt =>
-			//{
-			//	opt.UseNpgsql(connString);
-			//});
+				connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+			}
+			builder.Services.AddDbContext<DataContext>(opt =>
+			{
+				opt.UseNpgsql(connString);
+			});
 
 			builder.Services.AddSwaggerService();
 
