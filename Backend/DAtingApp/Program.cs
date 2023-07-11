efd209bce00a2b0012fd544970b97bc0613d3c61
tantawy997 <abdelrhman.eltantawy97@gmail.com>
2023-07-11 04:14:23 +0300

    deployment

--- Backend/DAtingApp/Program.cs
+++ Backend/DAtingApp/Program.cs
@@ -30,6 +30,33 @@ namespace DAtingApp
 			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 			
 			builder.Services.AddIdentityService(builder.Configuration);
+			var connString = "";
+			if (builder.Environment.IsDevelopment())
+				connString = builder.Configuration.GetConnectionString("co1");
+			else
+			{
+				// Use connection string provided at runtime by Heroku.
+				var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+				// Parse connection URL to connection string for Npgsql
+				connUrl = connUrl.Replace("postgres://", string.Empty);
+				var pgUserPass = connUrl.Split("@")[0];
+				var pgHostPortDb = connUrl.Split("@")[1];
+				var pgHostPort = pgHostPortDb.Split("/")[0];
+				var pgDb = pgHostPortDb.Split("/")[1];
+				var pgUser = pgUserPass.Split(":")[0];
+				var pgPass = pgUserPass.Split(":")[1];
+				var pgHost = pgHostPort.Split(":")[0];
+				var pgPort = pgHostPort.Split(":")[1];
+				var updatedHost = pgHost.Replace("flycast", "internal");
+
+				connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+			}
+			builder.Services.AddDbContext<DataContext>(opt =>
+			{
+				opt.UseNpgsql(connString);
+			});
+
 			builder.Services.AddSwaggerService();
 
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
